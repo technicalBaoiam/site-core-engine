7e4274a24c486a25f2561f0e34d4c93cf60e2a77
Avyakta000 <100035215+Avyakta000@users.noreply.github.com>
2024-09-29 04:22:17 +0530

    nexus minor changes

--- outreach/serializers.py
+++ outreach/serializers.py
@@ -15,6 +15,20 @@ class EnrollmentSerializer(serializers.ModelSerializer):
         fields = '__all__'
         # fields = ['id', 'student_full_name', 'email', 'phone_number', 'course', 'enrollment_type', 'enrollment_time']
 
+    def validate_student_full_name(self, value):
+        name_parts = value.split()
+        
+        # Check if there are at least two parts (first and last name)
+        if len(name_parts) < 2:
+            raise serializers.ValidationError("Full name must include at least a first name and a last name.")
+        
+        # ensure each part only contains alphabetic characters
+        for part in name_parts:
+            if not part.isalpha():
+                raise serializers.ValidationError("Full name must only contain alphabetic characters.")
+        
+        return value
+
 class ContactSerializer(serializers.ModelSerializer):
     class Meta:
         model = Contact
--- outreach/urls.py
+++ outreach/urls.py
@@ -4,6 +4,6 @@ from django.urls import path, include
 from .views import ContactCreateView, EnrollmentCreateView
 
 urlpatterns = [
-    path('contact/', ContactCreateView.as_view(), name='contact-create'),
+    path('contact-gcep/', ContactCreateView.as_view(), name='contact-create'),
     path('enrollment-query-save/', EnrollmentCreateView.as_view(), name='enrollment-query'),
 ]
